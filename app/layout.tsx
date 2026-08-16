b7cd988e3bd1c584fb3ca7ff756f114d25e6dea2
hilmyha <hilmyahmadhaidar@gmail.com>
2026-08-17 01:11:32 +0700

    first setup: react mdx, shadcnui, dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+
 import "./globals.css";
+import { ModeToggle } from "@/components/buttons/mode-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,8 +25,19 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <ModeToggle />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
